6a9809ac20963e0e6d2e2ff0ec7d6ff1b35c710e
小粽子 <862275679@qq.com>
2018-10-24 14:19:42 +0800

    gin

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	orm "github.com/omgzui/gin-demo/api/database"
+	db "github.com/omgzui/gin-demo/api/database"
 	router2 "github.com/omgzui/gin-demo/api/router"
 )
 
 func main() {
-	defer orm.Eloquent.Close()
+	defer db.Eloquent.Close()
 	router := router2.InitRouter()
 	router.Run(":8081")
 }
